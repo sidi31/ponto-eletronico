85220b8922030f0bed83245ba02958d04cc735c9
sidi31 <sidi.moreirap@gmail.com>
2025-12-03 00:03:42 -0300

    Delete ponto-completo directory

--- ponto-completo/backend/src/index.js
+++
@@ -1,4 +0,0 @@
-const app = require('./app');
-require('dotenv').config();
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
--- ponto-completo/backend/src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authCtrl = require('../controllers/authController');
-
-router.post('/login', authCtrl.login);
-router.post('/register-admin', authCtrl.registerAdmin); // use APENAS para inicializar em dev
-
-module.exports = router;
